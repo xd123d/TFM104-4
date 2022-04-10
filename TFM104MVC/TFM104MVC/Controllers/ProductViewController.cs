d1a70ddb225e41da4a8d6dab1361d8f36abb88fb
Midomommy <tinasue165@gmail.com>
2022-04-11 04:21:58 +0800

    商品頁加session
    為商品頁加上session(存pid)
    導到訂購頁並成功拿出來pid

--- TFM104MVC/TFM104MVC/Controllers/ProductViewController.cs
+++ TFM104MVC/TFM104MVC/Controllers/ProductViewController.cs
@@ -1,21 +1,24 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System;
 
 namespace TFM104MVC.Controllers
 {
     public class ProductViewController : Controller
     {
         //全部商品列表
-        public IActionResult productlist()
+        public IActionResult ProductList()
         {
             return View();
         }
 
         //單一商品
-        public IActionResult product([FromRoute] int id)
+        public IActionResult Product([FromRoute] Guid id)  //商品頁後面接商品id
         {
-            HttpContext.Session.SetString("camping_area_id", id.ToString());
-            HttpContext.Session.GetString("camping_area_id");
+            //把商品id存進session裡面，準備之後傳到其他地方(訂購頁,購物車 etc...)用
+            HttpContext.Session.SetString("pid", id.ToString());
+
+            //HttpContext.Session.GetString("camping_area_id");
             //Convert.ToInt32()
 
             return View();
--- TFM104MVC/TFM104MVC/Controllers/PurchaseController.cs
+++ TFM104MVC/TFM104MVC/Controllers/PurchaseController.cs
@@ -1,15 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 
 namespace TFM104MVC.Controllers
 {
     public class PurchaseController : Controller
     {
-        public IActionResult booking()
+        public IActionResult Booking()
         {
+            //把商品id從session拿出來
+            var pid= HttpContext.Session.GetString("pid");
+            //測試一下有沒有拿到對的id
+            System.Console.WriteLine(pid);
             return View();
         }
 
-        public IActionResult cart()
+        public IActionResult Cart()
         {
             return View();
         }
